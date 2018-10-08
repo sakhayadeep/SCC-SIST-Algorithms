064f47445c27c529cb625b221c097e0d43ee0bdd
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2018-10-08 20:54:37 +0530

    Added fibnocii.java

--- fibnocii/java/fibnocii.java
+++ fibnocii/java/fibnocii.java
@@ -1,1 +1,21 @@
-// add your code here :
+// Fibonacci series : 0 1 1 2 3 5 8 ....
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        int n = 10, // For first ten terms n=10 
+		 t1 = 0, 
+		 t2 = 1;
+        System.out.print("First " + n + " terms: ");
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
